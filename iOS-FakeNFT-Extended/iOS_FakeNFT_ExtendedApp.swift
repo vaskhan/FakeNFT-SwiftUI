2bff4e1d1ce706b6f81ec9d40624fbe07ef64045
Aziz Latipov <Latipov.Aziz@wb.ru>
2024-07-01 01:21:04 +0500

    Initial Commit

---
+++ iOS-FakeNFT-Extended/iOS_FakeNFT_ExtendedApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_FakeNFT_ExtendedApp.swift
+//  iOS-FakeNFT-Extended
+//
+//  Created by Aziz Latipov on 01.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct iOS_FakeNFT_ExtendedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
